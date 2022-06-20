5a223ba5c8dc847850c2409335c8393dc1b27f58
Angelo Longoria <42070067+arlongoria93@users.noreply.github.com>
2022-06-19 22:34:32 -0500

    finished hero content also settup up sanity config

---
+++ sanity.js
@@ -0,0 +1,17 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUserHook,
+  createClient,
+} from "next-sanity";
+import { isPropertyAccessChain } from "typescript";
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "06-19-2022",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
